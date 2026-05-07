4ab8dc7b6f26fef1177bdd7a7ca37ab3731ef014
Alejandro <alejandalmau@gmail.com>
2026-05-07 11:32:02 +0200

    Arreglar bug

--- Examen Repas/librocontable.js
+++ Examen Repas/librocontable.js
@@ -8,11 +8,11 @@ function main() {
     formulario.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        let fecha    = document.getElementById("fecha").value;
+        let fecha = document.getElementById("fecha").value;
         let concepto = document.getElementById("concepto").value;
-        let tipo     = document.getElementById("tipo").value;
-        let importe  = document.getElementById("importe").value;
-        let saldo    = calcularSaldo(tipo, importe);
+        let tipo = document.getElementById("tipo").value;
+        let importe = document.getElementById("importe").value;
+        let saldo = calcularSaldo(tipo, importe);
         agregarRegistro(fecha, concepto, tipo, importe, saldo);
         // Limpiar el formulario después de agregar el registro
         formulario.reset();
@@ -24,15 +24,15 @@ function main() {
 
 async function cargarDatos() {
     let informacio = await fetch('data.json');
-    datos          = await informacio.json()
+    datos = await informacio.json()
     console.log(datos);
-    guardarDatosStoragee(datos);
+    guardarDatosStorage(datos);
 }
 
 function guardarDatosStorage(datos) {
-    let oldData = JSON.parse(localStorage.getItem('datos')) || []; 
+    let oldData = JSON.parse(localStorage.getItem('datos')) || [];
     oldData.push(...datos);
-    localStorage.setItem('datos',JSON.stringify(oldData));
+    localStorage.setItem('datos', JSON.stringify(oldData));
 }
 
 function pintarDatos() {
@@ -41,55 +41,45 @@ function pintarDatos() {
     let tabla = document.getElementById('tabla-libro-contable');
 
     datosStorage.forEach(element => {
-        let fila      = document.createElement('tr');
-        
-        let borrar    = document.createElement('td');
+        let fila = document.createElement('tr');
+
+        let borrar = document.createElement('td');
         let btnBorrar = document.createElement('button');
         let btnBorrarText = document.createTextNode('Borrar');
         btnBorrar.appendChild(btnBorrarText);
         borrar.appendChild(btnBorrar);
         fila.appendChild(borrar);
 
-        let fecha     = document.createElement('td');
+        let fecha = document.createElement('td');
         let fechaNode = document.createTextNode(element.fecha);
         fecha.appendChild(fechaNode);
         fila.appendChild(fecha);
 
-        let concepto     = document.createElement('td');
+        let concepto = document.createElement('td');
         let conceptoNode = document.createTextNode(element.concepto);
         concepto.appendChild(conceptoNode);
         fila.appendChild(concepto);
 
-        let dh        = document.createElement('td');
-        let dhNode    = document.createTextNode(element.tipo);
+        let dh = document.createElement('td');
+        let dhNode = document.createTextNode(element.tipo);
         dh.appendChild(dhNode);
         fila.appendChild(dh);
 
-        let importe     = document.createElement('td');
+        let importe = document.createElement('td');
         let importeNode = document.createTextNode(element.importe);
         importe.appendChild(importeNode);
         fila.appendChild(importe);
 
-        let saldo     = document.createElement('td');
+        let saldo = document.createElement('td');
         let saldoNode = document.createTextNode(element.saldo);
         saldo.appendChild(saldoNode);
         fila.appendChild(saldo);
 
         tabla.appendChild(fila);
-        
+
     });
-    
+
 }
-
-
-
-
-
-
-
-
-
-
 
 function calcularSaldo(tipo, importe) {
     let saldoActual = document.getElementById("saldo-actual").innerText;
